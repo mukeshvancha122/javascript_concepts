ca6aaebe29feba8e1dc769f41c690cb6610d2d8d
Mukesh Reddy <mukeshvancha122@gmail.com>
2025-07-31 17:16:23 -0400

    Var scope

---
+++ fundamentals/VarScope.js
@@ -0,0 +1,13 @@
+// function example(){
+    
+//     console.log(a);
+//     var a=10;
+// } gives undefined
+
+function example(){
+    var a=10;
+    // var a=20
+    console.log(a);
+}
+
+example();
