b0b0a8206a09816e25da2eb7aa1d25c5dc498ca1
Mukesh Reddy <mukesh@aiowl.org>
2026-04-01 11:36:05 -0400

    Hoisitng

--- fundamentals/VarScope.js
+++ fundamentals/VarScope.js
@@ -4,10 +4,21 @@
 //     var a=10;
 // } gives undefined
 
-function example(){
-    var a=10;
-    // var a=20
+// function example(){
+//     var a=10;
+//     // var a=20
+//     console.log(a);
+// }
+
+// example();
+
+function scopeCheck(){
+    let a = 10;
+    var b = 20;
+    const c = 30;
     console.log(a);
 }
 
-example();
+console.log(a);
+console.log(b);
+console.log(c);
