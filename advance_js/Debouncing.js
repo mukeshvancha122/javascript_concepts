316e4e10a4117df2c7869db52c41b6355e385708
Mukesh Reddy <mukesh@aiowl.org>
2026-04-02 23:07:54 -0400

    Debouncing

---
+++ advance_js/Debouncing.js
@@ -0,0 +1,8 @@
+// Debouncing: Technique or actions only triggers after a certain series of events are done. 
+
+// Prevents unnecessary, frequent calls to a function—such as API requests or search 
+// filtering—by waiting until a set delay has passed since the last triggering event, improving performance and 
+// user experience
+
+// example: Search Input: Waiting until a user stops typing to trigger a search API 
+// request rather than searching on every keystroke.
